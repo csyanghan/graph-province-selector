1af7232d283bc9113d11927f9499fd472cc4c8c0
hanyang <shuerhy@163.com>
2019-03-31 14:23:11 +0800

    done

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
-    <h2>Yitu Prework</h2>
-    <!-- <ImageEditor></ImageEditor> -->
-    <Cascade></Cascade>
+    <h1>Yitu Prework</h1>
+    <button @click="choose('1')">图片操作</button>
+    <button @click="choose('2')">省市县选择器组件</button>
+    <keep-alive>
+      <component v-bind:is="currentTabComponent.name" v-bind="currentTabComponent.props"></component>
+    </keep-alive>
   </div>
 </template>
 
@@ -12,14 +15,63 @@ import Cascade from './components/Cascade.vue';
 
 export default {
   name: 'app',
+  data() {
+    return {
+      currentTabComponent: {
+        name: 'ImageEditor'
+      }
+    }
+  },
   components: {
     ImageEditor,
     Cascade
+  },
+  methods: {
+    choose(type) {
+      switch(type)
+      {
+        case '1':
+          this.currentTabComponent = {
+            name: 'ImageEditor'
+          };
+          break;
+        case '2':
+          this.currentTabComponent = {
+            name: 'Cascade',
+            props: {
+              level: 4
+            }
+          };
+          break;
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss">
+button {
+  margin: 10px;
+  display: inline-block;
+  white-space: nowrap;
+  cursor: pointer;
+  background: #fff;
+  border: 1px solid #dcdfe6;
+  color: #606266;
+  text-align: center;
+  box-sizing: border-box;
+  outline: none;
+  font-weight: 500;
+  padding: 12px 20px;
+  font-size: 14px;
+  border-radius: 4px;
+  transition: .1s;
+  &:hover {
+    color: #409eff;
+    border-color: #c6e2ff;
+    background-color: #ecf5ff;
+  }
+}
 * {
   padding: 0;
   margin: 0;
@@ -30,5 +82,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  padding-top: 30px;
+  padding-bottom: 30px;
+  h1 {
+    margin: 20px;
+  }
 }
 </style>
--- src/components/Cascade.vue
+++ src/components/Cascade.vue
@@ -1,25 +1,32 @@
 <template>
-  <div>
-    <input :value="selectedShow"/>
-    <div class="select-container">
-      <div class="tab" v-for="(item, index) in dataArr" :key="index" v-if="Object.prototype.toString.call(item) === '[object Array]'">
-        <h3 class="tab-active" v-if="!dataIndex[index]">请选择</h3>
-        <h3 class="tab-active" v-else>
-          <!-- <span v-if="index===0">{{dataArr[index][dataIndex[index]]}}</span> -->
-          <span>{{selectName(index)}}</span>
-        </h3>
-        <div class="tab-data" v-if="componentLevel === index">
-          <div v-for="(item2, index2) in dataArr[index]" :key="index2" class="tab-data-item" @click="chooseDistinct(index, index2)">
-            <span>{{item2.name}}</span>
+  <div class="container">
+    <div class="input-wrap">
+      <input :value="selectedShow" type="text" readonly autocomplete="off" placeholder="请选择" class="my-input" @click="choose"/>
+      <span class="down-icon" v-show="!opened"></span>
+      <span class="up-icon" v-show="opened"></span>
+    </div>
+    <transition name="slide-in-out">
+      <div class="select-container" v-show="opened">
+        <!-- eslint-disable-next-line -->
+        <div class="tab" v-for="(item, index) in dataArr" :key="index" v-if="Object.prototype.toString.call(item) === '[object Array]' && index < level">
+          <h3 class="tab-active" v-if="dataIndex[index] === undefined">请选择</h3>
+          <h3 class="tab-active" v-else @click="slectedChange(index)">
+            <span>{{selectName(index)}}</span>
+          </h3>
+          <div class="tab-data" v-if="componentLevel === index && (index+1)!==level">
+            <div v-for="(item2, index2) in dataArr[index]" :key="index2" class="tab-data-item" @click="chooseDistinct(index, index2)">
+              <span>{{item2.name}}</span>
+            </div>
           </div>
-        </div>
-        <div class="tab-data" v-if="componentLevel-1 === index && Object.prototype.toString.call(dataArr[componentLevel]) !== '[object Array]'">
-          <div v-for="(item2, index2) in dataArr[index]" :key="index2" class="tab-data-item" @click="chooseDistinct(index, index2)">
-            <span>{{item2.name}}</span>
+          <div class="tab-data" v-if="(index+1 === level || Object.prototype.toString.call(dataArr[index+1]) === '[object Object]')">
+            <!-- 为了显示最后一级 -->
+            <div v-for="(item2, index2) in dataArr[index]" :key="index2" class="tab-data-item" @click="chooseLastDistinct(index, index2)">
+              <span>{{item2.name}}</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -35,6 +42,7 @@ export default {
       dataIndex: [],
       componentLevel: 0,
       selected: [],
+      opened: false,
     }
   },
   props: ['level'],
@@ -44,44 +52,111 @@ export default {
     }
   },
   methods: {
+    choose() {
+      this.opened = !this.opened;
+    },
     chooseDistinct(index1, index2) {
       // index1 表示那一层级 index2 表示该层级下某一个项目
       if(Object.prototype.toString.call(this.dataArr[this.dataArr.length -1]) === '[object Array]') {
-      this.dataIndex[index1] = index2;
-      let nextLevel = null;
-      if(this.dataArr[index1][index2].children) {
-        nextLevel = this.dataArr[index1][index2].children.map(pro => ({
-          name: pro.name,
-          children: pro.children
-        }));
-      } else {
-        nextLevel = this.dataArr[index1][index2];
-      }
-      this.dataArr.push(nextLevel);
-      this.componentLevel ++;
+        this.dataIndex[index1] = index2;
+        let nextLevel = null;
+        if(this.dataArr[index1][index2].children) {
+          nextLevel = this.dataArr[index1][index2].children.map(pro => ({
+            name: pro.name,
+            children: pro.children
+          }));
+        } else {
+          nextLevel = this.dataArr[index1][index2];
+        }
+        this.dataArr.push(nextLevel);
+        this.componentLevel ++;
+        let rst = this.dataArr[index1][index2];
+        this.selected.splice(index1, 1, rst.name);
       }
     },
+    chooseLastDistinct(index1, index2) {
+      this.dataIndex.splice(index1, 1, index2);
+      this.selected.splice(index1,1 ,this.dataArr[index1][index2].name);
+      this.$emit('select-done', this.selected);
+    },
+    slectedChange(index) {
+      this.dataArr.splice(index+1);
+      this.dataIndex.splice(index);
+      this.selected.splice(index);
+      this.componentLevel = index;
+    },
     selectName(index) {
-      let first = this.dataArr[index];
-      let idx = this.dataIndex[index];
-      let second = first[idx];
-      const rst = second ? second.name : first.name;
-      this.selected[index] = rst;
-      return rst;
+      return this.selected[index];
     }
   },
   mounted() {
     this.dataArr.push(data);
-    console.log(data)
   },
 }
 </script>
 
 <style lang="scss" scoped>
+.slide-in-out-enter-active, .slide-in-out-leave-active {
+  transition: all .4s;
+}
+.slide-in-out-enter, .slide-in-out-leave-active {
+  transform: translate3d(0, 3rem, 0);
+  opacity: 0;
+}
+.container {
+  width: 800px;
+  margin: 0 auto;
+  position: relative;
+}
+.input-wrap {
+  margin-left: 10px;
+  position: relative;
+  cursor: pointer;
+  font-size: 14px;
+  line-height: 40px;
+  height: 40px;
+  min-width: 222px;
+  .down-icon {
+    display: inline-block;
+    width: 0;
+    height: 0;
+    border: 10px solid transparent;
+    border-top-color: #b4bac6;
+    position: absolute;
+    right: 5px;
+    top: 15px;
+  }
+  .up-icon {
+    display: inline-block;
+    width: 0;
+    height: 0;
+    border: 10px solid transparent;
+    border-bottom-color: #b4bac6;
+    position: absolute;
+    right: 5px;
+    bottom: 15px;
+  }
+  .my-input {
+    background-color: #fff;
+    border-radius: 4px;
+    border: 1px solid #dcdfe6;
+    box-sizing: border-box;
+    color: #606266;
+    display: inline-block;
+    height: 40px;
+    line-height: 40px;
+    outline: none;
+    padding: 0 15px;
+    width: 100%;
+    cursor: pointer;
+  }
+}
+
 .select-container {
   display: flex;
   width: 800px;
-  position: relative;
+  position: absolute;
+  top: 40px;
   .tab {
     .tab-active {
       color: #44b7b7;
@@ -92,9 +167,9 @@ export default {
         content: '';
         position: absolute;
         width: 100%;
-        bottom: -4px;
+        bottom: -6px;
         left: 0;
-        border: 1px solid #44b7b7;
+        border-top: 2px solid #44b7b7;
       }
     }
     h3 {
